bfc60ca0f45dd5de5773e1efeaa833341d9c5574
rbpolim <rbpolim@gmail.com>
2024-05-07 19:13:35 -0300

    feat: create put imc

--- app/api/imc/route.ts
+++ app/api/imc/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server"
 import { auth } from '@clerk/nextjs/server'
+import { NextResponse } from "next/server"
 
 import { prisma } from "@/lib/prisma"
 
@@ -49,7 +49,7 @@ export async function POST(
     })
 
     if (!profile) {
-      return new NextResponse('Weight is required', { status: 400 })
+      return new NextResponse('Profile not found', { status: 400 })
     }
 
     const formatHeight = height.replace(',', '.')
@@ -72,6 +72,60 @@ export async function POST(
     return NextResponse.json(imc)
   } catch (error) {
     console.error('[IMC_POST]', error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
+
+export async function PUT(
+  req: Request,
+) {
+  try {
+    const { userId } = auth()
+    const body = await req.json()
+
+    const { height, weight } = body
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    if (!height) {
+      return new NextResponse('Height is required', { status: 400 })
+    }
+
+    if (!weight) {
+      return new NextResponse('Weight is required', { status: 400 })
+    }
+
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: userId,
+      }
+    })
+
+    if (!profile) {
+      return new NextResponse('Profile not found', { status: 400 })
+    }
+
+    const formatHeight = height.replace(',', '.')
+    const formatWeight = weight.replace(',', '.')
+
+    const calculateIMC = formatWeight / (formatHeight * formatHeight) // Calculate IMC
+
+    const imc = await prisma.iMC.update({
+      where: {
+        profileId: profile.id,
+      },
+      data: {
+        heightInCentimeters: formatHeight * 100,
+        weightInGrams: formatWeight * 1000,
+        imc: calculateIMC,
+      }
+    })
+
+    return NextResponse.json(imc)
+  } catch (error) {
+    console.error('[IMC_PUT]', error)
     return new NextResponse('Internal Error', { status: 500 })
   }
 }
