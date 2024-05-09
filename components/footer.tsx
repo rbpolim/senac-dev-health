c7660be3cd43427edd7297ec739c8e60f12904d9
rbpolim <rbpolim@gmail.com>
2024-05-09 19:43:58 -0300

    chore: update main footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 export function Footer() {
   const date = new Date();
   const currentYear = date.getFullYear()
 
   return (
-    <footer className="backdrop-blur-sm flex items-center justify-center fixed bottom-0 h-12 border-t w-full bg-white">
-      <h2 className="text-muted-foreground">
-        SENAC © {currentYear} Dev.Health
+    <footer className="backdrop-blur-sm flex flex-col gap-1 items-center fixed bottom-0 p-4 border-t w-full bg-white">
+      <h2 className="text-sm ">
+        <Link href="/about" className="underline text-emerald-900">
+          SENAC
+        </Link>
+        {' '} © {currentYear} Dev.Health
       </h2>
+      <p className="text-xs text-muted-foreground">
+        Projeto Integrador: Parte 2
+      </p>
     </footer>
   )
 }
