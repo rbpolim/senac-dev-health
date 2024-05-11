2059e593e9767edfa2c29fce42154feb6446f4b7
rbpolim <rbpolim@gmail.com>
2024-05-10 22:29:26 -0300

    chore: remove useless code onboarding layout

--- app/(onboarding)/layout.tsx
+++ app/(onboarding)/layout.tsx
@@ -1,17 +1,8 @@
-import { redirect } from "next/navigation";
-import { currentUser } from "@clerk/nextjs/server";
-
 export default async function OnBoardingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const user = await currentUser();
-
-  // if (!user) {
-  //   redirect("/");
-  // }
-
   return (
     <main className="h-full">
       {children}
